5fb0d6e7573904ecb32a0ae9bf3d8eaa0a15ed09
techmovie <v.abed@icloud.com>
2024-10-13 14:58:22 +0800

    filter hupu tab

--- Scripts/hupu.js
+++ Scripts/hupu.js
@@ -29,9 +29,6 @@ if(url.includes("bbsallapi/lego/data")){
   })
 }else if(url.includes("device/v1/init")){
   obj.result.activityNav = [];
-  obj.result.ipLocation = '英国';
 }
-console.log(JSON.stringify(obj));
-$done({body: JSON.stringify(obj)})
 
-//https://games.mobileapi.hupu.com/3/8.0.88/bplapi/device/v1/init
+$done({body: JSON.stringify(obj)})
