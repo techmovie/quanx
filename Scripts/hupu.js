9b38eb77f67d354a60e0193c47bec65025731bb0
techmovie <v.abed@icloud.com>
2024-10-26 11:14:54 +0800

    fix: hupu tag

--- Scripts/hupu.js
+++ Scripts/hupu.js
@@ -9,7 +9,7 @@ if(url.includes("bbsallapi/lego/data")){
   })
 }else if(url.includes("news/v3/tab")){
   obj.result = obj.result.filter(tab=>{
-    return !tab.name.match(/球星|英雄/)
+    return tab.name.match(/赛程|数据|主队/)
   })
 }else if(url.includes("matchallapi/liveTabList")){
   obj.result.categoryList = obj.result.categoryList.filter(tab=>{
